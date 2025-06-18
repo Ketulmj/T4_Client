75b78a08a29b35c0b43d922911a2393efba84c18
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-18 22:52:00 +0530

    update Auth & NoAuth

--- src/middleware/Auth.jsx
+++ src/middleware/Auth.jsx
@@ -1,18 +1,18 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useUser } from "../contexts/user.context";
 import { userFetcher } from "../lib/userFetcher";
 
 const Auth = ({ children }) => {
   const [user, setUser] = useUser();
-  useEffect(() => {
+  // useEffect(() => {
     const fetchUser = async () => {
       if (!user?.UserId) {
         await userFetcher(user, setUser);
       }
     }
     fetchUser();
-  }, []);
+  // }, []);
   return user?.UserId ? children : <Navigate to="/login" replace />;
 };
 
