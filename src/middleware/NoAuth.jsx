0df38bdfd7b8a6e4c497bca48557d7d2dc3fef5b
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-18 22:31:55 +0530

    Update NoAuth.jsx

--- src/middleware/NoAuth.jsx
+++ src/middleware/NoAuth.jsx
@@ -6,9 +6,12 @@ import { userFetcher } from "../lib/userFetcher";
 const NoAuth = ({ children }) => {
   const [user, setUser] = useUser();
   useEffect(() => {
-    if (user.UserId == null) {
-      userFetcher(user, setUser);
+    const fetchUser = async () => {
+      if (!user?.UserId) {
+        await userFetcher(user, setUser);
+      }
     }
+    fetchUser();
   }, []);
   return user?.UserId ? children : <Navigate to="/dashboard" replace />;
 };
