7af419c82a83941bdf1fda23f85f5b960ab5d033
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-18 22:18:08 +0530

    Update Auth.jsx

--- src/middleware/Auth.jsx
+++ src/middleware/Auth.jsx
@@ -1,6 +1,16 @@
-import { Navigate } from 'react-router-dom'
-import Cookie from 'js-cookie'
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import { useUser } from "../contexts/user.context";
+import { userFetcher } from "../lib/userFetcher";
 
-const Auth = ({ children }) => Cookie.get('auth') ? children : <Navigate to='/login' />
+const Auth = ({ children }) => {
+  const [user, setUser] = useUser();
+  useEffect(() => {
+    if (user.UserId == null) {
+      userFetcher(user, setUser);
+    }
+  }, []);
+  return user?.UserId ? children : <Navigate to="/login" replace />;
+};
 
-export default Auth
+export default Auth;
