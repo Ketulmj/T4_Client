3374a6f0bfabdb21424ac08fd95364a2bbf37ced
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-20 12:09:58 +0530

    Update Auth.jsx

--- src/middleware/Auth.jsx
+++ src/middleware/Auth.jsx
@@ -1,18 +1,7 @@
-import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
-import { useUser } from "../contexts/user.context";
-import { userFetcher } from "../lib/userFetcher";
 
 const Auth = ({ children }) => {
-  const [user, setUser] = useUser();
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      await userFetcher(user, setUser);
-    };
-    fetchUser();
-  }, []);
-  return user?.userId ? children : <Navigate to="/login" />;
+  return localStorage.getItem('auth') ? children : <Navigate to="/login" />;
 };
 
 export default Auth;
