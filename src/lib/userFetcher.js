b471f4f4a48779fa6ce7276a1bd5d06fd1e04ee1
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-20 11:23:15 +0530

    Update userFetcher.js

--- src/lib/userFetcher.js
+++ src/lib/userFetcher.js
@@ -1,12 +1,18 @@
 export const userFetcher = (user, setUser) => {
     if (user.userId == null) {
+        const token = localStorage.getItem('auth');
+        if(token) {
         fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/get`,
-            {
-                method: 'GET',
-                credentials: "include",
-            }
-        )
-            .then(res => res.json())
-            .then(({ user }) => { setUser(user) })
-    };
+              method: 'POST',
+              // credentials: "include",
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: {
+                  token: JSON.stringify(token)
+              }
+        ).then(res => res.json())
+        .then(({ user }) => { setUser(user) })
+        };
+    }
 }
