535bf67b33579bc3e24bf06330fc0e15dde6a780
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-20 10:41:09 +0530

    Update NoAuth.jsx

--- src/middleware/NoAuth.jsx
+++ src/middleware/NoAuth.jsx
@@ -6,8 +6,8 @@ import { userFetcher } from "../lib/userFetcher";
 const NoAuth = ({ children }) => {
   const [user, setUser] = useUser();
   const token = localStorage.getItem('auth')
-  if(!token) {
-    return children;
+  if(token) {
+    return <Navigate to="/dashboard" />;
   }
   useEffect(() => {
     const fetchUser = async () => {
