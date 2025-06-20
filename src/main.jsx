b29c954a8021da859d2add0f9f6f018f52cddf4c
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-20 10:39:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -17,19 +17,19 @@ createRoot(document.getElementById('root')).render(
                         <Route path="/" element={<Home />} />
                         <Route path="/timetable" element={<TimeTableForm />} />
                         <Route path="/login" element={
-                        //    <NoAuth>
+                            <NoAuth>
                                 <Login />
-                            // </NoAuth>
+                            </NoAuth>
                         } />
                         <Route path="/signup" element={
-                        //    <NoAuth>
+                            <NoAuth>
                                 <Signup />
-                            // </NoAuth>
+                            </NoAuth>
                         } />
                         <Route path="/signup/:url" element={
-                            // <NoAuth>
+                            <NoAuth>
                                 <Signup />
-                            // </NoAuth>
+                            </NoAuth>
                         } />
                         <Route path="/dashboard" element={
                             <Auth>
