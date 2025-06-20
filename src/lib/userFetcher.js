5181175482fb3311a2750f2dffe975aaeda84086
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-20 12:43:05 +0530

    Update userFetcher.js

--- src/lib/userFetcher.js
+++ src/lib/userFetcher.js
@@ -1,24 +1,24 @@
-export const userFetcher = async (user, setUser) => {
-  if (!user?.userId) {
-    const token = localStorage.getItem('auth');
-    console.log(token);
-    if (token) {
-      try {
-        const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/get`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ token }),
-        });
-        const data = await res.json();
-        if (data?.user) {
-          console.log(data);
-          setUser(data.user);
-        }
-      } catch (err) {
-        console.error("Error fetching user:", err);
-      }
+export const userFetcher = async (setUser) => {
+  const token = localStorage.getItem('auth');
+  if (!token) return;
+
+  try {
+    const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/user/get`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ token }),
+    });
+    const data = await res.json();
+    if (data?.user) {
+      setUser(data.user);
+    } else {
+      setUser(null);
     }
+  } catch (err) {
+    console.error("Error fetching user:", err);
+    setUser(null);
   }
 };
+
