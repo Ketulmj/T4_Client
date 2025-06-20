c1a2454116ba3c42f605cb95f626b29f462d089c
Ketul Makwana <136818184+Ketulmj@users.noreply.github.com>
2025-06-20 12:01:04 +0530

    Update Auth.jsx

--- src/middleware/Auth.jsx
+++ src/middleware/Auth.jsx
@@ -9,7 +9,6 @@ const Auth = ({ children }) => {
   useEffect(() => {
     const fetchUser = async () => {
       await userFetcher(user, setUser);
-      setLoading(false);
     };
     fetchUser();
   }, []);
